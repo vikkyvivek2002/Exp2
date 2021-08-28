a79aab5d627c0154227b9b1679ab4373c4217cde
vivek vardhan . M <84318895+vikkyvivek2002@users.noreply.github.com>
2021-08-28 21:45:56 +0530

    Add files via upload

---
+++ Exp2.java
@@ -0,0 +1,17 @@
+public class Exp2 {
+
+	public static void main(String[] args) {
+		System.out.println("Str1");
+		try
+		{
+			System.out.println(10/0);
+		}
+		catch(Exception obj)
+		{
+		System.out.println(10/2);
+		}
+		System.out.println("Str2");
+
+	}
+
+}
